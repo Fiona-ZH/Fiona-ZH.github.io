5a920ab6ab58a12cd41cefa470ca5ceac019c193
fiona <fiona.feifei.zhang@gmail.com>
2018-06-02 19:03:46 -0400

    fixed sticky nav issue

--- scripts/home_nav.js
+++ scripts/home_nav.js
@@ -1,7 +1,8 @@
 window.onscroll = function() {stickyNav()};
 
 var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+// var sticky = navbar.offsetTop;
+var sticky = window.innerHeight;
 
 function stickyNav() {
   if (window.pageYOffset >= sticky) {
