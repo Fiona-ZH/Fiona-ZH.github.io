55dc464ec84af562d92572fed42a0f6bc8dc956c
fiona <fiona.feifei.zhang@gmail.com>
2018-06-06 10:59:44 -0400

    styled nav bar for desktop

--- scripts/home_nav.js
+++ scripts/home_nav.js
@@ -14,16 +14,16 @@ var sticky = window.innerHeight;
 function stickyNav() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
+    navbar.classList.remove("top_hidden")
     moveDown.classList.add("move_down")
   } else {
     navbar.classList.remove("sticky");
     moveDown.classList.remove("move_down")
-
+    navbar.classList.add("top_hidden")
   }
 }
 
-
-// ------ section indicator ------
+// ------ section indicator for max-width 1024px ------
 
 var projects = document.getElementById("navprojects");
 var proPos = document.getElementById("proPos").offsetTop;
@@ -37,19 +37,27 @@ var connPos = document.getElementById("connPos").offsetTop;
 function currentSele(){
   if(window.pageYOffset >= proPos && window.pageYOffset < aboutPos) {
     projects.classList.add("curr-sele");
+    projects.classList.add("nav__dot--focus-d");
   } else {
     projects.classList.remove("curr-sele");
+    projects.classList.remove("nav__dot--focus-d");
   }
 
   if(window.pageYOffset >= aboutPos && window.pageYOffset < connPos) {
     about.classList.add("curr-sele");
+    about.classList.add("nav__dot--focus-l");
   } else {
     about.classList.remove("curr-sele");
+    about.classList.remove("nav__dot--focus-l");
   }
 
   if(window.pageYOffset >= connPos) {
     connect.classList.add("curr-sele");
+    connect.classList.add("nav__dot--focus-d");
   } else {
     connect.classList.remove("curr-sele");
+    connect.classList.remove("nav__dot--focus-d");
   }
 }
+
+// ------ section indicator for min-width 1025px ------
